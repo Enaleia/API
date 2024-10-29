84222b6819f9b3b04c8f8b62c28c6839617329f1
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-10-29 09:44:34 -0400

    clean

--- business_logic/get_actions_and_materials_for_role.ts
+++ business_logic/get_actions_and_materials_for_role.ts
@@ -1,5 +1,7 @@
 import { get_actions, get_materials } from '../routes/';
 
+
+// roles: ["Manufacturer", "Administrator", "Internal", "Fisherman", "Recycler", "Port Coordinator"]
 export const get_actions_and_materials_for_role = async (client: any, roles: string[]) => {
   try {
     const actions = await get_actions(client, roles)
